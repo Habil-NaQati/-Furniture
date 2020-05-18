7be40bb2a0e84d29c1d777c93f843bda0534308f
Habil-NaQati <habil.naqati@gmail.com>
2020-05-18 18:12:48 +0530

    So far, I have only developed the front-page!

---
+++ Index.php
@@ -0,0 +1,513 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Company Name Here Interior | Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="" />
+  
+  <!-- css files -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
+    <link href="css/style.css" rel='stylesheet' type='text/css' /><!-- custom css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
+  <!-- //css files -->
+
+
+<!-- Add icon library -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+  <script defer src="https://use.fontawesome.com/releases/v5.6.3/js/regular.js" integrity="sha384-V+AkgA1cZ+p3DRK63AHCaXvO68V7B5eHoxl7QVN21zftbkFn/sGAIVR7vmQL3Zhp" crossorigin="anonymous"></script>
+
+  <script defer src="https://use.fontawesome.com/releases/v5.6.3/js/brands.js" integrity="sha384-VLgz+MgaFCnsFLiBwE3ItNouuqbWV2ZnIqfsA6QRHksEAQfgbcoaQ4PP0ZeS0zS5" crossorigin="anonymous"></script>
+
+  <script defer src="https://use.fontawesome.com/releases/v5.6.3/js/fontawesome.js" integrity="sha384-treYPdjUrP4rW5q82SnECO7TPVAz4bpas16yuE9F5o7CeBn2YYw1yr5oC8s8Mf8t" crossorigin="anonymous"></script>
+
+  
+  <link href="css/css_slider.css" type="text/css" rel="stylesheet" media="all">
+
+  <!-- google fonts -->
+  <link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+  <!-- //google fonts -->
+  
+</head>
+<body>
+<!-- //header -->
+<header class="py-sm-3 pt-3 pb-2" id="home">
+  <div class="container">
+    <!-- nav -->
+    <div class="top d-md-flex">
+      <div id="logo">
+        <h1> <a href="index.html"><span class="fas fa-chair"></span> Company Name Here</a></h1>
+      </div>
+      <div class="search-form mx-md-auto">
+        <div class="n-right-w3ls">
+          <form action="#" method="post" class="newsletter">
+            <input class="search" type="text" placeholder="Search..." required="">
+            <button class="form-control btn" value=""><span class="fa fa-search"></span></button>
+          </form>
+        </div>
+      </div>
+      <div class="forms mt-md-0 mt-2">
+        <a href="login.html" class="btn"><span class="far fa-user-circle"></span> Sign In</a>
+        <a href="register.html" class="btn"><span class="fa fa-pencil-square-o"></span> Create Account</a>
+      </div>
+    </div>
+    <nav class="text-center">
+      <label for="drop" class="toggle"><span class="fa fa-bars"></span></label>
+      <input type="checkbox" id="drop" />
+      <ul class="menu">
+        <li class="mr-lg-4 mr-2 active"><a href="index.html">Home</a></li>
+        <li class="mr-lg-4 mr-2"><a href="about.html">About Us</a></li>
+        <li class="mr-lg-4 mr-2"><a href="services.html">Services</a></li>
+        <li class="mr-lg-4 mr-2"><a href="categories.html">Categories</a></li>
+        <li class="mr-lg-4 mr-2"><a href="blog.html">Blog</a></li>
+        <li class=""><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+    <!-- //nav -->
+  </div>
+</header>
+<!-- //header -->
+
+<!-- banner -->
+<section class="banner_w3lspvt">
+  <div class="csslider infinity" id="slider1">
+    <input type="radio" name="slides" checked="checked" id="slides_1" />
+    <input type="radio" name="slides" id="slides_2" />
+    <input type="radio" name="slides" id="slides_3" />
+    <input type="radio" name="slides" id="slides_4" />
+    <ul>
+      <li>
+        <div class="banner-top">
+          <div class="overlay">
+            <div class="container">
+              <div class="w3layouts-banner-info text-center">
+                <h3 class="text-wh">We Are the best designers of Interior Furniture</h3>
+                <p class="text-li mx-auto mt-2">paragraph here</p>
+                <a href="about.html" class="button-style mt-4">Explore More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="banner-top1">
+          <div class="overlay">
+            <div class="container">
+              <div class="w3layouts-banner-info text-center">
+                <h3 class="text-wh">We Are the best designers of Interior Furniture</h3>
+                <p class="text-li mx-auto mt-2">paragraph here</p>
+                <a href="about.html" class="button-style mt-4">Explore More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="banner-top2">
+          <div class="overlay">
+            <div class="container">
+              <div class="w3layouts-banner-info text-center">
+                <h3 class="text-wh">We Are the best designers of Interior Furniture</h3>
+                <p class="text-li mx-auto mt-2">paragraph here</p>
+                <a href="about.html" class="button-style mt-4">Explore More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="banner-top3">
+          <div class="overlay">
+            <div class="container">
+              <div class="w3layouts-banner-info text-center">
+                <h3 class="text-wh">We Are the best designers of Interior Furniture</h3>
+                <p class="text-li mx-auto mt-2">paragraph here</p>
+                <a href="about.html" class="button-style mt-4">Explore More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="arrows">
+      <label for="slides_1"></label>
+      <label for="slides_2"></label>
+      <label for="slides_3"></label>
+      <label for="slides_4"></label>
+    </div>
+  </div>
+</section>
+<!-- //banner -->
+<!---728x90--->
+
+<!-- about-->
+<section class="wthree-row py-5" id="about">
+  <div class="container py-lg-5">
+    <div class="row about-main">
+      <div class="col-lg-5 about-text-grid text-left pr-lg-5">
+        <div class="title-desc">
+          <h3 class="main-title-w3pvt text-capitalize">Our Mission is to design the best interior around</h3>
+        </div>
+        <hr>
+        <p class="mt-3">paragraph here</p>
+
+        <a href="#more" class="text-capitalize serv_link btn bg-theme1 scroll mt-4">view more</a>
+      </div>
+      <div class="col-lg-3 col-sm-6 about-right mt-lg-0 mt-4">
+        <img src="images/a3.jpg" class="img-fluid" alt="" />
+      </div>
+      <div class="col-lg-4 col-sm-6 mt-lg-0 mt-3 about-right2">
+        <img src="images/a1.jpg" class="img-fluid" alt="" />
+      </div>
+    </div>
+  </div>
+</section>
+<!-- //about -->
+<!---728x90--->
+
+<!-- what we do -->
+<section class="what-we-do">
+  <div class="overlay1 py-5">
+    <div class="container py-lg-5">
+      <div class="row">
+        <div class="col-lg-3 mb-lg-0 mb-4">
+          <h3 class="heading">What <span>we do</span></h3>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-md-0 mt-4">
+          <div class="grid">
+            <div class="grid-layout">
+              <span class="fa fa-building mb-sm-4 mb-2" aria-hidden="true"></span>
+              <h5 class="card-title mb-sm-3 mb-2">Architecture</h5>
+              <p class="card-text">paragraph here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-md-0 mt-sm-5 mt-4">
+          <div class="grid">
+            <div class="grid-layout">
+              <span class="fa fa-bath mb-sm-4 mb-2" aria-hidden="true"></span>
+              <h5 class="card-title mb-sm-3 mb-2">Interior Design</h5>
+              <p class="card-text">paragraph here</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-md-0 mt-sm-5 mt-4">
+          <div class="grid">
+            <div class="grid-layout">
+              <span class="fa fa-align-left mb-sm-4 mb-2" aria-hidden="true"></span>
+              <h5 class="card-title mb-sm-3 mb-2">Furniture</h5>
+              <p class="card-text">paragraph here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- //what we do -->
+<!---728x90--->
+
+<!-- specialization-->
+<section class="wthree-row specialization py-5">
+  <div class="container py-lg-5">
+    <div class="row about-main">
+      <div class="col-lg-5 about-text-grid text-left pr-lg-5">
+        <h3 class="heading"> Our Specialization </h3>
+        <p class="mt-3">paragraph here</p>
+        <hr>
+        <p>01. Home Furniture Design</p>
+        <p>02. Commercial Office Design</p>
+        <p>03. Interior Furniture Design</p>
+        <p>04. Indoor and Outdoor Furniture</p>
+        <p>05. Landscape Design</p>
+      </div> 
+      <div class="col-lg-3 col-sm-6 mt-lg-0 mt-4">
+        <span>24</span>
+        <h4>years of experience</h4>
+      </div>
+      <div class="col-lg-4 col-sm-6 mt-lg-0 mt-4 about-right2">
+        <img src="images/specialization.jpg" class="img-fluid" alt="" />
+      </div>
+    </div>
+  </div>
+</section>
+<!-- //specialization -->
+
+<!-- top categories -->
+<section class="news py-5" id="news">
+  <div class="container py-lg-5">
+    <div class="d-flex view">
+      <h3 class="heading mb-5"> Top Categories </h3>
+      <div class="ml-auto">
+        <a href="categories.html">View All</a>
+      </div>
+    </div>
+    <div class="row news-grids">
+      <div class="col-lg-3 col-sm-6 newsgrid1">
+        <img src="images/living-room.jpg" alt="news image" class="img-fluid">
+        <h4 class="mt-4">Living Room</h4>
+        <p class="mt-3">paragraph here</p>
+      </div>
+      <div class="col-lg-3 col-sm-6 mt-sm-0 mt-5 newsgrid2">
+        <img src="images/dining-room.jpg" alt="news image" class="img-fluid">
+        <h4 class=" mt-4">Dining Room</h4>
+        <p class="mt-3">paragraph here</p>
+      </div>
+      <div class="col-lg-3 col-sm-6 newsgrid3 mt-lg-0 mt-4">
+        <img src="images/bedrooom.jpg" alt="news image" class="img-fluid">
+        <h4 class=" mt-4">Bedroom</h4>
+        <p class="mt-3">paragraph here</p>
+      </div>
+      <div class="col-lg-3 col-sm-6 newsgrid3 mt-lg-0 mt-4">
+        <img src="images/office-room.jpg" alt="news image" class="img-fluid">
+        <h4 class=" mt-4">Office Room</h4>
+        <p class="mt-3">paragraph here</p>
+      </div>
+    </div>    
+  </div>
+</section>
+<!-- //top categories --> 
+
+<!-- tabs -->
+<section class="middile-sec py-5" id="tabs">
+  <div class="container py-md-5">
+    <div class="row">
+      <div class="col-lg-12 middile-inner-con">
+        <div class="tab-main mx-auto">
+
+          <input id="tab1" type="radio" name="tabs" checked>
+          <label for="tab1"> Soft Furniture</label>
+
+          <input id="tab2" type="radio" name="tabs">
+          <label for="tab2"> Kitchen Cabinet</label>
+
+          <input id="tab3" type="radio" name="tabs">
+          <label for="tab3"> Three Seater Sofa</label>
+          
+          <input id="tab4" type="radio" name="tabs">
+          <label for="tab4"> Wall Design</label>
+
+          <section id="content1">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="images/soft.jpg" class="img-fluid" alt="" />
+              </div>
+              <div class="col-md-8 mt-md-0 mt-4">
+                <h5 class="mb-3 text-capitalize">The personality and lifestyle of a homeowner is reflected to a large extent in the interior design and décor of the home. Soft furnishings play an important role in interiors.</h5> 
+                <p>paragraph here</p>
+              </div>
+            </div>
+          </section>
+
+          <section id="content2">
+            <div class="row">
+              <div class="col-md-8">
+                <h5 class="mb-3 text-capitalize">Kitchen cabinets are the built-in furniture installed in many kitchens for storage of food, cooking equipment, and often silverware and dishes for table service. </h5>
+                <p>paragraph here</p>
+              </div>
+              <div class="col-md-4 mt-md-0 mt-4">
+                <img src="images/kitchen.jpg" class="img-fluid" alt="" />
+              </div>
+            </div>
+          </section>
+
+          <section id="content3">
+            <div class="row">
+              <div class="col-md-4">
+                <img src="images/sofa.jpg" class="img-fluid" alt="" />
+              </div>
+              <div class="col-md-8 mt-md-0 mt-4">
+                <h5 class="mb-3 text-capitalize">The best decor for your living room are 3 Seater Sofas so choose wisely according to your home requirements. Explore our latest sofa designs. </h5>
+                <p>paragraph here</p>
+              </div>
+            </div>
+          </section>
+          
+          <section id="content4">
+            <div class="row">
+              <div class="col-md-8">
+                <h5 class="mb-3 text-capitalize">wooden walls are a desirable, but not always economical, option. Keep the warmth and glow of wood inside your home. </h5>
+                <p>paragraph here</p>
+              </div>
+              <div class="col-md-4 mt-md-0 mt-4">
+                <img src="images/wall.jpg" class="img-fluid" alt="" />
+              </div>
+            </div>
+          </section>
+
+        </div>
+      </div>
+      <div class="col-lg-6 about-img mt-lg-0 mt-4">
+      </div>
+    </div>
+  </div>
+</section>
+<!--//tabs -->
+
+<!-- footer -->
+<footer class="footer py-5">
+  <div class="container py-md-3">
+    <div class="footer-grid_section text-center">
+      <div class="footer-title mb-3">
+        <h2> <a href="index.html"><span class="fas fa-chair"></span> Company Name Here</a></h2>
+      </div>
+      <div class="footer-text">
+        <p>paragraph here</p>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-lg-4 mb-lg-0 mb-4 footer-top">
+        <h4 class="mb-4 w3f_title text-uppercase">Contact Info</h4>
+        <div class="footer-style-w3ls my-2">
+          <p> contact details here</p>
+        </div>
+        <div class="footer-style-w3ls my-2">
+          <p> phone number here</p>
+        </div>
+        <div class="footer-style-w3ls">
+          <p> <a href="mailto:info@examplemail.com">info@examplemail.com</a></p>
+        </div>
+      </div>
+      <div class="col-lg-2 col-md-3 col-6 footv3-left">
+        <h4 class="mb-md-4 mb-3 w3f_title text-uppercase">Company</h4>
+        <ul class="list-agileits">
+          <li class="my-2">
+            <a href="about.html">
+              About Us
+            </a>
+          </li>
+          <li class="mb-2">
+            <a href="#">
+              Terms of use
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="#">
+              Faq's
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="#">
+              Privacy Ploicy
+            </a>
+          </li>
+          <li>
+            <a href="contact.html">
+              Get In Touch
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-2 col-md-3 col-6">
+        <h4 class="mb-md-4 mb-3 w3f_title text-uppercase">Categories</h4>
+        <ul class="list-agileits">
+          <li class="my-2">
+            <a href="#">
+              Furniture Chairs
+            </a>
+          </li>
+          <li class="mb-2">
+            <a href="#">
+              Three Seater Sofas
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="#">
+              Dining Tables
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="#">
+              Office Chairs
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Kitchen Cabinets
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-lg-2 col-md-3 col-6 mt-md-0 mt-sm-5 mt-4">
+        <h4 class="mb-md-4 mb-3 w3f_title text-uppercase">Resources</h4>
+        <ul class="list-agileits">
+          <li class="my-2">
+            <a href="#">
+              Getting Started
+            </a>
+          </li>
+          <li class="mb-2">
+            <a href="#">
+              Best Collections
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="categories.html">
+              All Categories
+            </a>
+          </li>
+          <li class="my-2">
+            <a href="#">
+              24/7 Support
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Contact for Help
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+      <div class="col-lg-2 col-md-3 col-6 mt-md-0 mt-sm-5 mt-4">
+        <h4 class="mb-md-4 mb-3 w3f_title text-uppercase">Account</h4>
+        <ul class="list-agileits">
+          <li class="my-2">
+            <a href="login.html">
+              Sign In
+            </a>
+          </li>
+          <li class="">
+            <a href="register.html">
+              Create Account
+            </a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </div>
+  <!-- //footer bottom -->
+</footer>
+<!-- //footer -->
+
+<!-- copyright -->
+<section class="copy-right py-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <p class="">© 2019 Company Name Here. All rights reserved | Design by
+          <a href=""> company here</a>
+        </p>
+      </div>
+      <div class="col-md-4 mt-md-0 mt-4">
+        <div class="subscribe-form">
+          <form action="#" method="post" class="newsletter">
+            <input class="subscribe" type="text" placeholder="Subscribe..." required="">
+            <button class="form-control btn" value=""><span class="fa fa-long-arrow-alt-right"></span></button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- copyright -->
+
+<!-- move top icon -->
+<a href="#home" class="move-top text-center"></a>
+<!-- //move top icon -->
+  
+</body>
+</html>
